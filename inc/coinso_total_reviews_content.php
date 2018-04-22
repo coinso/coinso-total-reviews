62d4ab8820df45a371b93a46df85bcfa0ae89934
ido barnea <barbareshet@gmail.com>
2018-04-22 11:03:22 +0300

    Added Service to schema

--- inc/coinso_total_reviews_content.php
+++ inc/coinso_total_reviews_content.php
@@ -13,10 +13,15 @@ global $ctr_options, $reviews_atts;
         'reviews_url'       =>  $ctr_options['reviews_url'] ? $ctr_options['reviews_url'] : '',
     ), $args);
 ?>
-
-<div itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">
-    <span itemprop="ratingValue"><?php echo $reviews_atts['total_score'];?></span> stars -
-    <span itemprop="reviewCount"><?php echo $reviews_atts['total_reviews'];?></span> reviews
+<div itemscope itemtype="http://schema.org/Service">
+    <meta itemprop="serviceType" content="Towing & Roadside Assistance" />
+    <span itemprop="provider" itemscope itemtype="http://schema.org/AutomotiveBusiness">
+        <span itemprop="name">Swift Towing</span>
+    </span>
+    <div itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">
+        <span itemprop="ratingValue"><?php echo $reviews_atts['total_score'];?></span> stars -
+        <span itemprop="reviewCount"><?php echo $reviews_atts['total_reviews'];?></span> reviews
+    </div>
 </div>
 <div class="review_us">
     <a href="<?php echo $reviews_atts['reviews_url'];?>" target="_blank" rel="nofollow">Review Us</a>
